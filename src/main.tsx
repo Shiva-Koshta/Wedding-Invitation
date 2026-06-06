1471ec9f9ff5a0218a650a38e7c29f8b0cc5fe1f
Shiva-Koshta <114857995+Shiva-Koshta@users.noreply.github.com>
2026-06-06 14:31:23 +0530

    changed the font

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import '@fontsource/cormorant-garamond/400.css'
 import '@fontsource/cormorant-garamond/500.css'
 import '@fontsource/cormorant-garamond/600.css'
-import '@fontsource/pinyon-script/400.css'
+import '@fontsource/great-vibes/400.css'
 import './index.css'
 import App from './App.tsx'
 
